afc5aaee932f036ba9a60608c60795a41e367b6f
Nazar <nazar.drahusevych@gmail.com>
2023-09-01 16:26:29 +0300

    Complete katas Numbers and StringCleaning

---
+++ CodeWarsTasks/kata_6/Number.cs
@@ -0,0 +1,24 @@
+﻿using System.Linq;
+
+namespace CodeWarsTasks.kata_6
+{
+    public class Number
+    {
+        public static int DigitalRoot(long n)
+        {
+            while (n >= 10)
+            {
+                int sum = 0;
+                while (n != 0)
+                {
+                    sum += (int)(n % 10);
+                    n /= 10;
+                }
+
+                n = sum;
+            }
+            return (int)n;
+        }
+        
+    }
+}
--- CodeWarsTasks/kata_8/StringCleaning.cs
+++ CodeWarsTasks/kata_8/StringCleaning.cs
@@ -1,7 +1,11 @@
-﻿namespace CodeWarsTasks.kata_8
+﻿using System.Linq;
+
+namespace CodeWarsTasks.kata_8
 {
     public class StringCleaning
     {
+        public static string StringClean(string str) => str.Where(t => !char.IsDigit(t))
+            .Aggregate("", (current, t) => current + t);
         
     }
 }
