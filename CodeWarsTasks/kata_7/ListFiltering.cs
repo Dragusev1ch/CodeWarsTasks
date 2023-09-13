fb964cae5f7d1f54fa6e764b2d00efcdd91c8614
Nazar <nazar.drahusevych@gmail.com>
2023-09-13 11:06:26 +0300

    complete task ListFiltering

--- CodeWarsTasks/kata_7/ListFiltering.cs
+++ CodeWarsTasks/kata_7/ListFiltering.cs
@@ -1,7 +1,10 @@
-﻿namespace CodeWarsTasks.kata_7
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace CodeWarsTasks.kata_7
 {
     public class ListFiltering
     {
-        
+        public static IEnumerable<int> GetIntegersFromList(List<object> listOfItems) => listOfItems.OfType<int>();
     }
 }
